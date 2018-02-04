3df09fcb7d500c3e95f44373c01726e36a7a36f0
Fred Choi <koreanwglasses@gmail.com>
2018-02-04 16:05:53 -0500

    linting

--- MusicLab/src/scripts/audio/Synthesizer.js
+++ MusicLab/src/scripts/audio/Synthesizer.js
@@ -9,22 +9,22 @@ function Synthesizer() {
 
 Synthesizer.prototype.test = function() {
     let _self = this;
-    let loop_0 = function(time) {
-        _self.synth.triggerAttackRelease("C4", "4n", time + 0);
-        _self.synth.triggerAttackRelease("Eb4", "4n", time + 0.5);
-        _self.synth.triggerAttackRelease("G4", "4n", time + 1);
-        _self.synth.triggerAttackRelease("Bb4", "4n", time + 1.5); 
-        _self.synth.triggerAttackRelease("C4", "4n", time + 2);
-        _self.synth.triggerAttackRelease("Eb4", "4n", time + 2.5);
-        _self.synth.triggerAttackRelease("G4", "4n", time + 3);
-        _self.synth.triggerAttackRelease("Bb4", "4n", time + 3.5); 
-    }
+    let loop0 = function(time) {
+        _self.synth.triggerAttackRelease('C4', '4n', time + 0);
+        _self.synth.triggerAttackRelease('Eb4', '4n', time + 0.5);
+        _self.synth.triggerAttackRelease('G4', '4n', time + 1);
+        _self.synth.triggerAttackRelease('Bb4', '4n', time + 1.5);
+        _self.synth.triggerAttackRelease('C4', '4n', time + 2);
+        _self.synth.triggerAttackRelease('Eb4', '4n', time + 2.5);
+        _self.synth.triggerAttackRelease('G4', '4n', time + 3);
+        _self.synth.triggerAttackRelease('Bb4', '4n', time + 3.5);
+    };
 
-    Tone.Transport.schedule(loop_0, 0);
+    Tone.Transport.schedule(loop0, 0);
     Tone.Transport.loopEnd = '1m';
     Tone.Transport.loop = true;
 
     Tone.Transport.start();
-}
+};
 
-export { Synthesizer };
+export {Synthesizer};
