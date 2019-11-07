4effe4347b3bdf85cdfbf1603fe3c329e7edd05b
Andrew Hunn <ahunn@umich.edu>
2019-11-07 12:18:45 -0500

    Update Extends

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
+	"root": true, /* restrict to this level */
 	"parserOptions": {
 		"ecmaVersion": 6,
 		"sourceType": "module"
 	},
 	"parser": "babel-eslint",
-	"extends": "google"
+	"extends": [
+		"./node-modules/eslint-config-google/index.js",
+		"./node-modules/eslint-config-xo/index.js"
+		]
 };
